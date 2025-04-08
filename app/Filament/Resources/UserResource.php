fe37d85facf9b29917abc0b9ba34fe5aa56227fb
Daniel Heinze <danielheinze96@gmail.com>
2025-04-08 13:06:30 +0200

    Filament COnfiguration #1

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
+
+final class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                self::getAvatarFileUploadComponent()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                SpatieMediaLibraryImageColumn::make('avatar')
+                    ->collection('avatars')
+                    ->circular()
+                    ->extraImgAttributes(['loading' => 'lazy']),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('avatar_url')
+                    ->wrap()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()->visible(Auth::user()->id === 1),
+                Tables\Actions\DeleteAction::make()->visible(Auth::user()->id === 1),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageUsers::route('/'),
+        ];
+    }
+
+    public static function getAvatarFileUploadComponent(): SpatieMediaLibraryFileUpload
+    {
+        return SpatieMediaLibraryFileUpload::make('avatars')
+            ->responsiveImages()
+            ->collection('avatars')
+            ->conversion('thumb')
+            ->hiddenLabel()
+            ->alignCenter()
+            ->image()
+            ->imageEditor()
+            ->circleCropper()
+            ->avatar()
+            ->directory('avatars')
+            ->disk('public')
+            ->afterStateHydrated(function ($state, callable $set, callable $get, $record): void {
+                if ($record) {
+                    $record->avatar_url = $record->getFilamentAvatarUrl();
+                    $record->save();
+                }
+            });
+    }
+}
